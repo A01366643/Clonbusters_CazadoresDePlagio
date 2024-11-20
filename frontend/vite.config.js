0055a38a3b45c5b98cb88940a068cafa6cf35bde
A01366643 <80075513+A01366643@users.noreply.github.com>
2024-11-19 18:21:10 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Clonbusters_CazadoresDePlagio/', // Debe coincidir exactamente con el nombre de tu repositorio
+  base: './', // Cambia esto a ruta relativa
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
 })
