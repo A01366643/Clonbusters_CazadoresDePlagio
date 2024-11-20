971cf4ef9822e48188868e7b508d770f3e7bf743
A01366643 <80075513+A01366643@users.noreply.github.com>
2024-11-19 18:17:02 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ClonBusters_CazadoresDePlagio/', 
-  root: '.',  // Asegura que Vite busque en el directorio correcto
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/Clonbusters_CazadoresDePlagio/', // Debe coincidir exactamente con el nombre de tu repositorio
 })
